677ad521218f3c6d18849e1877066af09116b9a9
DDM <dimaddm18@yandex.ru>
2017-02-07 11:59:44 +0300

    Todo mark added

--- tests/neural_network_test.py
+++ tests/neural_network_test.py
@@ -1,32 +1,29 @@
-# coding=utf-8
 
 import numpy, cv2, sys, os
 from neural import NeuralNetwork
 
 
-# Чтение изображений из папки
+# Reading images from folder
 def read_images(folder):
     # type: (str) -> list(tuple)
     ext = {".jpg", ".jpeg", ".png", ".bmp"}
     files = []
-    # Загрузка файлов из папки
     dirs = os.listdir(folder)
 
-    # Убираем лишние файлы
     for d in dirs:
-        # Изображения разбиты по классам
-        # Классы это папки
+        # Reading each folder
+        # One folder == One class
         path = os.path.join(folder, d)
         if os.path.isdir(path):
             files_list = os.listdir(path)
             for f in files_list:
                 for xt in ext:
-                    # Проверяем формат файла
+                    # Check file extension
                     full_path = os.path.join(path, f)
                     if full_path.endswith(xt):
-                        # Записываем путь до файла + класс(класс==имя папки)
+                        # Write path + class name
                         files.append((full_path, d))
-    # Чтение изображений
+    # Loading images
     im_data = []
     for (fl, cls) in files:
         image = cv2.imread(fl, cv2.IMREAD_GRAYSCALE)
@@ -39,9 +36,10 @@ def setup_images(im_data, (width, height)):
 
     for im in im_data:
         (image, fl) = im
-        # Изображения для нейронки должны быть одного размера
+        # FIXME:  What's first? Resize || normalization?
+        # Resizing(for neural network)
         rs_image = cv2.resize(image, (width, height))
-        # Нормализуем
+        # Normalization
         norm_image = cv2.normalize(rs_image, alpha=-1, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
         result.append((norm_image, fl))
     return result
