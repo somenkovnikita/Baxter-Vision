8356c468dbe92a6f22ea2e5d962b50178731adb3
geeknik96 <geeknik96@gmail.com>
2017-04-13 15:44:35 +0300

    Add first work cube detector with look at(Commited by DDM from GUAP local PC)

--- calibration/lookat.py
+++ calibration/lookat.py
@@ -1,11 +1,18 @@
 import cv2
 from baxter import LimbMover
 
+
+import baxter_interface
+# import baxter_external_device
+#
+# from baxter_interface import CHECK_VERSION
+
 mover = None
 
-dc = 0.05
+dc = 0.03
 x = 0.5
 y = 0.0
+z = 0.1
 
 def set_and_log(coord, name):
     coord += dc
@@ -15,6 +22,10 @@ lookAt = False
 x_px = 0
 y_px = 0
 cascade = None
+
+grip = None
+
+
 
 def mouse_callback(event, x, y, flags, param):
     global lookAt, x_px, y_px
@@ -28,10 +39,13 @@ def mouse_callback(event, x, y, flags, param):
 
 
 def init(config):
-    global mover,x ,y, cascade
+    global mover,x ,y, cascade, grip
 
     cv2.namedWindow('Aim')
     cv2.setMouseCallback('Aim', mouse_callback)
+
+    grip = baxter_interface.Gripper('left')
+    grip.reset_custom_state()
 
     cascade = cv2.CascadeClassifier()
     if cascade.load(config):
@@ -39,15 +53,19 @@ def init(config):
 
     mover = LimbMover.LimbMover('left')
 
+
     pass
 
 
+aim_x = 0.56
+aim_y = 0.45
+
 def draw_aim(frame):
+    global aim_x, aim_y
     resolution = frame.shape[:2]
     resolution = resolution[1], resolution[0]
 
-    aim_x = 0.5
-    aim_y = 0.5
+    print aim_x, aim_y
 
     x = int(aim_x * resolution[0])
     y = int(aim_y * resolution[1])
@@ -59,6 +77,29 @@ def draw_aim(frame):
     cv2.line(frame, p3, p4, (255, 255, 255))
 
 
+def get_z(z):
+    return -330.0 * z + 201.0
+
+old_z = z
+def down():
+    global mover, old_z
+    pose = mover._get_current_pose()
+    old_z, pose[2] = pose[2], -0.12
+    mover.move(pose, move=True)
+
+def up():
+    global mover, old_z
+    pose = mover._get_current_pose()
+    pose[2] = old_z
+    mover.move(pose, move=True)
+
+def take():
+    global grip
+    grip.close()
+
+def uptake():
+    global grip
+    grip.open()
 
 
 def lookat(frame):
@@ -66,58 +107,93 @@ def lookat(frame):
 
     draw_aim(cframe)
 
-    global mover, x, dc, y, lookAt, x_px, y_px
+    global mover, x, dc, y, z, lookAt, x_px, y_px, aim_x, aim_y
 
     key = cv2.waitKey(10) & 0xff
     if key == ord('w'):
         x += dc
         print 'x =', x
+        mover.move([x, y, z, -3.14, 0.0, 0.0], move=True)
     elif key == ord('s'):
         x -= dc
         print 'x =', x
+        mover.move([x, y, z, -3.14, 0.0, 0.0], move=True)
     elif key == ord('d'):
         y -= dc
         print 'y =', y
+        mover.move([x, y, z, -3.14, 0.0, 0.0], move=True)
     elif key == ord('a'):
         y += dc
         print 'y =', y
+        mover.move([x, y, z, -3.14, 0.0, 0.0], move=True)
     elif key == ord('t'):
         mover.move([0.5, 0.0, 0.1, -3.14, 0.0, 0.0], move=True)
-    elif lookAt:
+    elif key == ord('g'):
+        down()
+    elif key == ord('h'):
+        up()
+    elif key == ord('j'):
+        aim_x -= 0.01
+    elif key == ord('l'):
+        aim_x += 0.01
+    elif key == ord('i'):
+        aim_y -= 0.01
+    elif key == ord('k'):
+        aim_y += 0.01
+    elif key == ord('n'):
+        take()
+    elif key == ord('m'):
+        uptake()
+    elif key == ord('z'):
+        z += dc
+        if mover.move([x, y, z, -3.14, 0.0, 0.0], move=True):
+            print 'z =', z
+        else:
+            print 'Not suc z down'
+    elif key == ord('x'):
+        z -= dc
+        if mover.move([x, y, z, -3.14, 0.0, 0.0], move=True):
+            print 'z =', z
+        else:
+            print 'Not suc z down'
+    if lookAt:
         h, w = cframe.shape[:2]
-        x = 0.157 / 172 * (y_px - h / 2)
-        y = 0.157 / 172 * (x_px - w / 2)
-        print x, y
+        z_px = get_z(z)
+        x = 0.157 / z_px * (y_px - h * aim_y)
+        y = 0.157 / z_px * (x_px - w * aim_x)
+        print x, y, z, z_px
         lookAt = False
 
         pose = mover._get_current_pose()
         pose[0] += x
         pose[1] += y
+        pose[2] = z
         print pose
         mover.move(pose, move=True)
-    elif cascade:
-        cubes = cascade.detectMultiScale(cframe)
-        h, w = cframe.shape[:2]
-        if len(cubes) > 0:
-            print 'Found ', len(cubes), 'rects'
-
-            y_px = cubes[0][3] / 2 + cubes[0][1]
-            x_px = cubes[0][2] / 2 + cubes[0][0]
-
-            x = 0.157 / 172 * (y_px - h / 2)
-            y = 0.157 / 172 * (x_px - w / 2)
-            print x, y
-            lookAt = False
-
-            pose = mover._get_current_pose()
-            pose[0] += x
-            pose[1] += y
-            print pose
-            mover.move(pose, move=True)
-        for x, y, w, h in cubes:
-            p = x, y
-            rect = x + w, y + h
-            cv2.rectangle(cframe, p, rect, (255, 255, 255), 2)
+    if cascade:
+        pass
+        # cubes = cascade.detectMultiScale(cframe)
+        # h, w = cframe.shape[:2]
+        # if len(cubes) > 0:
+        #     print 'Found ', len(cubes), 'rects'
+        #
+        #     y_px = cubes[0][3] / 2 + cubes[0][1]
+        #     x_px = cubes[0][2] / 2 + cubes[0][0]
+        #
+        #     x = 0.157 / 172 * (y_px - h / 2)
+        #     y = 0.157 / 172 * (x_px - w / 2)
+        #     print x, y
+        #     lookAt = False
+        #
+        #     pose = mover._get_current_pose()
+        #     pose[0] += x
+        #     pose[1] += y
+        #     print pose
+        #     mover.move(pose, move=True)
+        # for x, y, w, h in cubes:
+        #     p = x, y
+        #     rect = x + w, y + h
+        #     cv2.rectangle(cframe, p, rect, (255, 255, 255), 2)
 
     cv2.imshow('Aim', cframe)
     cv2.waitKey(100)
