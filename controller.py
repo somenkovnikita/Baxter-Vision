4a8add1126a3d075fe20a107623682d9ae630f59
Nikita Somenkov <somenkov.nikita@icloud.com>
2017-05-04 21:07:49 +0300

    Update training_set and test_set

--- controller.py
+++ controller.py
@@ -1,5 +1,25 @@
-def start_controller():
+import argparse
+
+
+class RobotController:
+    def __init__(self):
+        self.cube_detector = None
+        self.letter_detector = None
+
+    def run(self):
+        pass
+
+    
+def start_controller(aim_word):
     raise Exception('Not working(')
 
 if __name__ == "__main__":
-    start_controller()
+    parser = argparse.ArgumentParser(
+        description='Main script for play cube')
+
+    parser.add_argument('-w', '--word', required=True,
+                        help='Aim word')
+
+    arguments = parser.parse_args()
+
+    start_controller(arguments.word)
